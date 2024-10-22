385565eeb83b7f390f76d1e760a3e7aeb7d7b04f
saadchaudhaary <saad@zirrusone.com>
2024-10-22 13:56:33 +0530

    feat: Add child table data to CRM Deal in get_deal method
    - Enhanced the get_deal function to retrieve and include child table data for CRM Deal.
    - Modified query to return all child tables linked to CRM Deal, including their respective records.
    - Added support for fetching contacts, fields metadata, form script, and assigned users for the deal.

--- crm/fcrm/doctype/crm_deal/api.py
+++ crm/fcrm/doctype/crm_deal/api.py
@@ -20,6 +20,20 @@ def get_deal(name):
 		frappe.throw(_("Deal not found"), frappe.DoesNotExistError)
 	deal = deal.pop()
 
+	# Get all child table doctypes linked to CRM Deal
+	meta = frappe.get_meta("CRM Deal")
+	child_tables = [df for df in meta.fields if df.fieldtype in ["Table","Table MultiSelect" ] ]
+
+	deal['child_tables'] = {}
+
+	for child_table in child_tables:
+		child_doctype = child_table.options
+		child_records = frappe.get_all(
+			child_doctype,
+			fields="*",
+			filters={"parent": deal['name']}
+		)
+		deal['child_tables'][child_table.fieldname] = child_records
 
 	deal["contacts"] = frappe.get_all(
 		"CRM Contacts",
