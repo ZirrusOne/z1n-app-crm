83e3d5eb2bdd458f7c9a29fd704506fc65ce5471
saadchaudhaary <saad@zirrusone.com>
2024-10-25 01:15:41 +0530

    Sales Status Summary

---
+++ crm/fcrm/report/sales_status_summary/sales_status_summary.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Status Summary"] = {
+	"filters":[
+        {
+            "fieldname": "deal_owner",
+            "label": "Deal Owner",
+            "fieldtype": "Link",
+            "options": "User",
+            "reqd": 0
+        },
+        {
+            "fieldname": "from_date",
+            "label": "From Date",
+            "fieldtype": "Date",
+            "reqd": 0
+		},
+        {
+            "fieldname": "to_date",
+            "label": "To Date",
+            "fieldtype": "Date",
+            "reqd": 0        
+		},
+        {
+            "fieldname": "crm_organization",
+            "label": "CRM Organization",
+            "fieldtype": "Link",
+            "options": "CRM Organization",
+            "reqd": 0
+        }
+    ]
+};
