07b3d6933cba4039b697c29ae058c53a6ae26ed2
priyank <priyank@zirrusone.com>
2024-11-25 16:56:22 +0530

    change tage color

--- frontend/src/components/Icons/PriceTagIcon.vue
+++ frontend/src/components/Icons/PriceTagIcon.vue
@@ -2,7 +2,7 @@
   <!-- SVG for price tag as shown in mockup -->
 <svg version="1.1" id="Layer_1" xmlns="SVG namespace " xmlns:xlink="XLink namespace "
 	 viewBox="0 0 32 32" xml:space="preserve">
-<polygon style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;" points="26,15.589 26,6 16.411,6 5,17.507
+<polygon style="fill:none;stroke:#7e368d;stroke-width:2;stroke-miterlimit:10;" points="26,15.589 26,6 16.411,6 5,17.507
 	14.493,27 "/>
 <circle cx="22" cy="10" r="1"/>
 </svg>
