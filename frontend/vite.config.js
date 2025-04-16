8cdad153d1404b7c4b942e1cbbc7ab4fd02acf84
JoshuaR <36710177+joshuarestivo@users.noreply.github.com>
2025-04-16 19:00:47 +0000

    fix: address frappe cloud build issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    frappeui(),
+    frappeui({
+      buildConfig: {
+        indexHtmlPath: path.resolve(__dirname, 'index.html'),
+      },
+    }),
     vue({
       script: {
         propsDestructure: true,
